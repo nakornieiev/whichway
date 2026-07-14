79bf5eb9cdbec7f315f847d7c75c568b5b1f0d3c
Vlad Nakornieiev <66900494+VladNakornieiev@users.noreply.github.com>
2026-07-14 09:59:36 +0200

    feat: adding tests for detect_manager by path

--- src/shim_detect.rs
+++ src/shim_detect.rs
@@ -16,3 +16,40 @@ pub fn detect_manager(path: &Path, content: &str) -> Option<ManagerInfo> {
 
     None
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn detect_asdf_by_path() {
+        let path = Path::new("/home/user/.asdf/shims/python");
+
+        let manager_info = detect_manager(&path, "");
+        assert_eq!(manager_info, Some(ManagerInfo::Asdf));
+    }
+
+    #[test]
+    fn detect_nvm_by_path() {
+        let path = Path::new("/home/user/.nvm/shims/node");
+
+        let manager_info = detect_manager(&path, "");
+        assert_eq!(manager_info, Some(ManagerInfo::Nvm));
+    }
+
+    #[test]
+    fn detect_pyenv_by_path() {
+        let path = Path::new("/home/user/.pyenv/shims/python");
+
+        let manager_info = detect_manager(&path, "");
+        assert_eq!(manager_info, Some(ManagerInfo::Pyenv));
+    }
+
+    #[test]
+    fn detect_no_manager() {
+        let path = Path::new("/home/user/.some_manager/shims/some_package");
+
+        let manager_info = detect_manager(&path, "");
+        assert_eq!(manager_info, None);
+    }
+}
